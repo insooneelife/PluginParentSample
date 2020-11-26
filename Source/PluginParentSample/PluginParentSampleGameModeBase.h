8407840d766157514c3a375c38a05d0435eecb42
unknown <insooneelife@naver.com>
2020-11-26 19:49:50 +0900

    first commit

---
+++ Source/PluginParentSample/PluginParentSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginParentSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINPARENTSAMPLE_API APluginParentSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
